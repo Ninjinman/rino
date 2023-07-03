098aa7f20aeb9cddb1bce61e9b88f0c0508c05ec
DESKTOP-6AOTDLR\copco <copcop77@gmail.com>
2023-07-03 21:52:23 +0900

    トマト定数化、ロジックコメントアウト

--- rino/views.py
+++ rino/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from .consts import TOMATO_RATE
+
+#設定レベルを定義(0～5)
+SETTING_RANK = 0
 
 def slot_game(request):
     return render(request, 'rino/slot_game.html')
 
 def game(request):
-    settingtmt = [293.8,284.9,276.5,260.0,243.6,233.2]
+    # トマト遷移確率を出力
+    tomatoRate = TOMATO_RATE[SETTING_RANK]
+
+    # 1プレイヤー情報取得(今はどの状態？)from DB
+
+    # 2プレイヤー情報が取得できなかったら(初期状態にする)
+
+    # 3次の状態の計算
+        # 現在の状態によってロジック変わる switch文
+        # 現在が基本B(0)なら　1/tomatoRateでトマト化 みたいな
+
+    # 4状態に応じた出目の計算
+        # 中身はようわからん
+    
+    # 5 3で計算した状態と、4で計算した出目をテーブルに保存
+
+    return JsonResponse({"result": "Success."})
+
     
